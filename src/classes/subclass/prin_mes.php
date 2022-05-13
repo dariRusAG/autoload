08921e4e84801c69e72578bf78812dfc194ecdcf
dariRusAG <91362737+dariRusAG@users.noreply.github.com>
2022-05-14 09:17:14 +1000

    Update prin_mes.php

--- src/classes/subclass/prin_mes.php
+++ src/classes/subclass/prin_mes.php
@@ -2,10 +2,10 @@
 namespace classes\subclass;
 class print_mes
 {
-    public static function print_message($file){
+    public static function print_message($JSON){
         // Если файл существует - получаем его содержимое
-        if (file_exists($file)){
-            $content = json_decode(file_get_contents($file));
+        if (file_exists($JSON)){
+            $content = json_decode(file_get_contents($JSON));
             foreach($content->messages as $message){
                 echo "<p>$message->date $message->user: $message->message</p>";
             }} else echo "История сообщений пуста :(</p>";
